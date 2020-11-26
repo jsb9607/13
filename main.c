01079e0157f92ae39ed1b1f73f3d81d893112638
jsb9607 <57170950+jsb9607@users.noreply.github.com>
2020-11-26 16:48:53 +0900

    13_03

--- main.c
+++ main.c
@@ -1,25 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "keywords.h"
+#include "keywords.c"
 
-struct student{
+int is_whitespace(char c)
+{
+	if(c == ' ' || c == '\r' || c == '\n' || c == '\t')	
+	{
+		return 1;
+	}
+	return 0;
+}
 
-	int ID;
-	char name[20];
-	float grade;
-};
 
+
+int fget_word(FILE *fp, char* word)
+{
+	char c;
+	int cnt;
+
+	while((c = fgetc(fp))!=EOF)
+	{
+		if(is_whitespace(c) == 0)	break;
+	}
+	if(c == EOF)	return 0;
+
+	cnt = 0;
+	word[cnt++] = c;
+	word[cnt] = '\0';
+
+	while((word[cnt] = fgetc(fp))!=EOF)
+	{
+		if(is_whitespace(word[cnt]) == 1)
+		{
+			word[cnt] = '\0';
+			break;
+		}
+		cnt++;
+	}	
+	return cnt;
+		
+}
 
 int main(void) {
 
-	struct student s1 = {1711181, "Seobin", 4.3};
+	FILE *fp;
+	char fname[100];
+	char word[100];
+	int cnt;
 
-	s1.ID = 1234567;
-	s1.name[20] = "Juyeop";
-	s1.grade = 4.2;
+	printf("열 파일 입력하시오 : ");
+	scanf("%s", fname);
 
-	printf("ID : %d/", s1.ID);
-	printf("name : %s", s1.name);
-	printf("grade : %f/n" s1.grade);
+
+	fp = fopen(fname, "r");
+
+	if(fp == NULL)
+	{
+		printf("파일을 못열음 (%s)\n", fname);
+		return -1;
+	}
+
+	while(fget_word(fp, word) != 0)
+	{
+		count_word(word);
+	}
+	print_word();
+	
+
+	fclose(fp);
 	
 	return 0;
 }
